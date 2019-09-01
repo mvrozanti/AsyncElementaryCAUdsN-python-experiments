15262f41e0859ba5e7ff9d74e3b939313408c7d6
Marcelo <mvrozanti@hotmail.com>
2019-08-31 23:22:41 -0300

    primeira implementação

--- impl/py/async-eca.py
+++ impl/py/async-eca.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
-rule = 40
+import code
+rule = 30
 # rule set:
 # 111 110 101 100 011 010 001 000
 #  x   x   x   x   x   x   x   x
-rule_set = {int(x) for x in bin(rule)[2:][::-1]}
-
-w = 5
-t = 30
-space = [[0,0,1,0,0]]
-
-print(rule_set)
+rule_transitions = [int(x) for x in format(rule,'#010b')[2:][::-1]]
+t = w = 10
+spacetime = [[0]*w]; spacetime[-1][w//2-1] = 1
 
 for i in range(t):
-    new_space = []
-    for x in range(len(space[-1])):
-        for rule in rule_set:
-            pass
+    future_space = []
+    space = spacetime[-1]
+    for x in range(len(space)):
+        l_n = space[x-1]
+        m_n = space[x]
+        r_n = space[(x+1)%w]
+        ix = 7 - (l_n << 2 + ((m_n << 1) + r_n))
+        future_space += [rule_transitions[ix]]
+        print(future_space)
+        code.interact(local=globals().update(locals()) or globals())
+    spacetime += [future_space]
+
+for timestep in spacetime:
+    print(timestep)
