594fa095dfe6ba9e121dbb7c7711eda278e5a337
Marcelo <mvrozanti@hotmail.com>
2019-09-09 15:19:22 -0300

    teste corrigido para começar da regra 1

--- impl/test.py
+++ impl/test.py
@@ -14,10 +14,13 @@ def test_normal_sync_on_async_context():
 @pytest.mark.skipif(sys.maxsize < 0xffffffff, reason='Py_ssize_t too small for this test')
 def test_for_conservative_rule_with_scheme():
     schemes = read_schemes_from_file('async-update-schemes.json')
-    for rule in range(255):
+    for rule in range(1,255):
         conservative = True
         for scheme in schemes:
             if not is_rule_conservative(rule, 30, 30, scheme):
                 conservative = False
                 break
         print('Rule', rule, 'is', '' if conservative else 'not', 'conservative')
+
+if __name__ == '__main__':
+    test_for_conservative_rule_with_scheme()
