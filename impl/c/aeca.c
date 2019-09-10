d43a193dcbdb875cd638564e25f49f2da4874f44
Marcelo <mvrozanti@hotmail.com>
2019-09-10 18:53:36 -0300

    gen_space_combo traduzido

--- impl/c/aeca.c
+++ impl/c/aeca.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h> 
+#include<math.h>
 
 const unsigned int W = 10;
 const unsigned int T = 10;
@@ -40,8 +41,23 @@ bool is_spacetime_conservative(bool** spacetime, int w, int t){
     return true;
 }
 
+bool** gen_space_combo(int w){
+    unsigned int mask = 1U << 7;
+    bool** space_combo = malloc(pow(2,w));
+    for(int i=0; i < pow(2,w); i++){
+        bool* space = malloc(8);
+        int z = w;
+        for (int j = 0; j < 8; j++) {
+            space[j] = (w & mask) ? 1 : 0;
+            z <<= 1;
+        }
+        space_combo[i] = space;
+    }
+    return space_combo;
+}
+
 bool is_rule_conservative(int rule, int t, int w, int* ranks){
-    
+
 
 }
 
