4045579783822a2413a72fc5254ba745127aaac4
--local <--global>
2019-10-10 23:53:04 -0300

    Alterada forma de input das regras

--- impl/javascript/ac.js
+++ impl/javascript/ac.js
@@ -1,33 +1,27 @@
 var gen0 = [0,0,0,0,0,1,0,0,0,0,0];
 
-var ruleNumb1 = 0;
-var ruleNumb2 = 0;
-var ruleNumb3 = 0;
-var ruleNumb4 = 1;
-var ruleNumb5 = 1;
-var ruleNumb6 = 1;
-var ruleNumb7 = 1;
-var ruleNumb8 = 0;
+var ruleNumb = '00011110';
+var splitedRuleNumb = ruleNumb.split('').map(Number);
 
 function generate(param) {
 	return param.map(function(content, item, array) {
 		if(array[item-1]===undefined) {
 			array[item-1]=array[10];
-			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=ruleNumb1 : array[item-1]===1 && content===1 && array[item+1]===0 ? content=ruleNumb2 :
-			array[item-1]===1 && content===0 && array[item+1]===1 ? content=ruleNumb3 : array[item-1]===1 && content===0 && array[item+1]===0 ? content=ruleNumb4 : 
-			array[item-1]===0 && content===1 && array[item+1]===1 ? content=ruleNumb5 : array[item-1]===0 && content===1 && array[item+1]===0 ? content=ruleNumb6 :
-			array[item-1]===0 && content===0 && array[item+1]===1 ? content=ruleNumb7 : array[item-1]===0 && content===0 && array[item+1]===0 ? content=ruleNumb8 : null;
+			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[0] : array[item-1]===1 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[1] :
+			array[item-1]===1 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[2] : array[item-1]===1 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[3] : 
+			array[item-1]===0 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[4] : array[item-1]===0 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[5] :
+			array[item-1]===0 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[6] : array[item-1]===0 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[7] : null;
 		} else if (array[item+1]===undefined) {
 			array[item+1]=array[0];
-			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=ruleNumb1 : array[item-1]===1 && content===1 && array[item+1]===0 ? content=ruleNumb2 :
-			array[item-1]===1 && content===0 && array[item+1]===1 ? content=ruleNumb3 : array[item-1]===1 && content===0 && array[item+1]===0 ? content=ruleNumb4 : 
-			array[item-1]===0 && content===1 && array[item+1]===1 ? content=ruleNumb5 : array[item-1]===0 && content===1 && array[item+1]===0 ? content=ruleNumb6 :
-			array[item-1]===0 && content===0 && array[item+1]===1 ? content=ruleNumb7 : array[item-1]===0 && content===0 && array[item+1]===0 ? content=ruleNumb8 : null;
+			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[0] : array[item-1]===1 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[1] :
+			array[item-1]===1 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[2] : array[item-1]===1 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[3] : 
+			array[item-1]===0 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[4] : array[item-1]===0 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[5] :
+			array[item-1]===0 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[6] : array[item-1]===0 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[7] : null;
 		} else {
-			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=ruleNumb1 : array[item-1]===1 && content===1 && array[item+1]===0 ? content=ruleNumb2 :
-			array[item-1]===1 && content===0 && array[item+1]===1 ? content=ruleNumb3 : array[item-1]===1 && content===0 && array[item+1]===0 ? content=ruleNumb4 : 
-			array[item-1]===0 && content===1 && array[item+1]===1 ? content=ruleNumb5 : array[item-1]===0 && content===1 && array[item+1]===0 ? content=ruleNumb6 :
-			array[item-1]===0 && content===0 && array[item+1]===1 ? content=ruleNumb7 : array[item-1]===0 && content===0 && array[item+1]===0 ? content=ruleNumb8 : null;
+			return array[item-1]===1 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[0] : array[item-1]===1 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[1] :
+			array[item-1]===1 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[2] : array[item-1]===1 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[3] : 
+			array[item-1]===0 && content===1 && array[item+1]===1 ? content=splitedRuleNumb[4] : array[item-1]===0 && content===1 && array[item+1]===0 ? content=splitedRuleNumb[5] :
+			array[item-1]===0 && content===0 && array[item+1]===1 ? content=splitedRuleNumb[6] : array[item-1]===0 && content===0 && array[item+1]===0 ? content=splitedRuleNumb[7] : null;
 		}
 	})
 }
